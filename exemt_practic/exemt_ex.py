555d8215b2a1355bc7d272b53be93dc0983e7352
yoni <yoniddya@gmail.com>
2026-02-21 21:38:28 +0200

    הוספת קובץ טקסט

--- exemt_practic/exemt_ex.py
+++ exemt_practic/exemt_ex.py
@@ -2,11 +2,11 @@ def filter_excellent_students(file_name, file_autput_name = None):
     with open(file_name , "r", encoding="utf-8") as file:
         lines = file.readlines()
         # print(lines)
-        processed_students = []
-        for line in lines :
-            line = line.strip()
-            name,grade_str = line.split(",")
-            grade = int(grade_str)
-            processed_students.append(name, grade)
-        print(processed_students)
+    processed_students = []
+    for line in lines :
+        line = line.strip()
+        name,grade_str = line.split(",")
+        grade = int(grade_str)
+        processed_students.append(name, grade)
+    print(processed_students)
 filter_excellent_students("students.txt")
