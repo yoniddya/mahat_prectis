ab938d787b4e04ca64d8423a368baf70e2a88ab0
yoni <yoniddya@gmail.com>
2026-02-21 22:40:21 +0200

    פרויקט בשלבי בניייה

--- exemt_practic/exemt_ex.py
+++ exemt_practic/exemt_ex.py
@@ -1,12 +1,30 @@
-def filter_excellent_students(file_name, file_autput_name = None):
+def filter_excellent_students(file_name, file_autput_name):
     with open(file_name , "r", encoding="utf-8") as file:
         lines = file.readlines()
         # print(lines)
     processed_students = []
     for line in lines :
         line = line.strip()
-        name,grade_str = line.split(",")
-        grade = int(grade_str)
-        processed_students.append(name, grade)
+        if not line:
+            continue
+        if "," not in line:
+            continue
+        try:
+            name,grade_str = line.split(",")
+            grade = int(grade_str)
+            processed_students.append((name, grade))
+        except ValueError:
+            continue
+    excellent_students =[]
+    for name , grade in processed_students:
+        if grade >= 90 :
+            excellent_students.append(name)
+        else:
+            continue
+    with open(file_autput_name, "w" ,encoding="utf-8") as f :
+        for name in excellent_students:
+            f.write(name + "\n")
     print(processed_students)
-filter_excellent_students("students.txt")
+    print("file redus secssed ", file_autput_name)
+
+filter_excellent_students("students.txt", "excellent.txt")
